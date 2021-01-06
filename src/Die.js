13e53e208c8ea5fed1abc54525f0d8a41e418584
root <dcdev001@gmail.com>
2021-01-06 18:10:46 +0000

    minor changes

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+	  numberWords : ["one", "two", "three", "four", "five", "six"],
+	  val: 5
+  };
+
   constructor(props) {
 	  super(props);
 	  this.handleClick = this.handleClick.bind(this);
@@ -12,14 +17,17 @@ class Die extends Component {
   }
 	
   render() {
+	const {numberWords, locked, val, disabled, rolling} = this.props;
+	let classes = `Die fas fa-dice-${numberWords[val-1]} fa-5x `;
+	if(locked) classes+="Die-locked ";
+	if(rolling) classes+="Die-rolling";
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+      <i
+        className={classes}
         onClick={this.handleClick}
+		disabled={disabled}
       >
-        {this.props.val}
-      </button>
+      </i>
     );
   }
 }
